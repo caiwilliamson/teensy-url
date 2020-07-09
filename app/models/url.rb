4b8456ab092aa98c310e181b9534ab391be0c18d
Cai Williamson <cai.mistry.williamson@gmail.com>
2020-07-09 22:07:54 +0100

    Validate slugs with a regex

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,10 @@
 class Url < ApplicationRecord
+  SLUG_REGEX = /\A[-A-Za-z0-9]+\z/
+
   before_validation :create_slug
 
   validates :original, presence: true
-  validates :slug, presence: true, uniqueness: true
+  validates :slug, presence: true, uniqueness: true, format: { with: SLUG_REGEX }
 
   def create_slug
     self.slug = SecureRandom.uuid[0..5] if self.slug.blank?
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
+  let(:slug_regex) { Url::SLUG_REGEX }
+
   it "is not valid without an original url" do
     url = Url.create(original: nil)
     expect(url).to_not be_valid
@@ -13,6 +15,10 @@ RSpec.describe Url, type: :model do
       expect(url).to be_valid
     end
 
+    it "has a valid slug" do
+      expect(url.slug).to match(slug_regex)
+    end
+
     it "has a unique slug" do
       url2 = Url.create(original: "https://website.com")
 
@@ -23,6 +29,14 @@ RSpec.describe Url, type: :model do
   it "is not valid if the slug is already taken" do
     url1 = Url.create(original: "https://website.com", slug: "abcde")
     url2 = Url.create(original: "https://website.com", slug: "abcde")
+
+    expect(url1).to be_valid
+    expect(url2).to_not be_valid
+  end
+
+  it "is not valid if the slug contains invalid characters" do
+    url1 = Url.create(original: "https://website.com", slug: "aBcDe-")
+    url2 = Url.create(original: "https://website.com", slug: "abcde$")
 
     expect(url1).to be_valid
     expect(url2).to_not be_valid
