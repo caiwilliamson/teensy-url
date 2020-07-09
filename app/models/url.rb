9c6831e95a85c4e7a685866af4c4869e3667e505
Cai Williamson <cai.mistry.williamson@gmail.com>
2020-07-09 23:16:23 +0100

    Trim trailing/leading whitespace of original urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ApplicationRecord
   SLUG_REGEX = /\A[-A-Za-z0-9]+\z/
 
-  before_validation :create_slug
+  before_validation :trim_original_url, :create_slug
 
   validates :original, presence: true, url: true
   validates :slug, presence: true, uniqueness: true, format: { with: SLUG_REGEX }
@@ -11,6 +11,11 @@ class Url < ApplicationRecord
   end
 
   private
+    # Trim leading and trailing whitespace.
+    def trim_original_url
+      self.original.strip! if self.original
+    end
+
     def create_slug
       self.slug = SecureRandom.uuid[0..5] if self.slug.blank?
     end
