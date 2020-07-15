fdc565c98e22a7634b0eb72e6176206bcdf0034b
Cai Williamson <cai.mistry.williamson@gmail.com>
2020-07-15 15:40:14 +0100

    Unassign non-custom (generated) slugs on validation failure

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,8 @@
 class Url < ApplicationRecord
   SLUG_REGEX = /\A[-A-Za-z0-9]+\z/
 
-  before_validation :trim_original_url, :create_slug
+  before_validation :trim_original_url, :assign_slug
+  after_validation :unassign_slug, if: Proc.new { not_custom_slug? && self.errors.any? }
 
   validates :original, presence: true, url: true
   validates :slug, presence: true, uniqueness: true, format: { with: SLUG_REGEX }
@@ -16,7 +17,15 @@ class Url < ApplicationRecord
       self.original.strip! if self.original
     end
 
-    def create_slug
-      self.slug = SecureRandom.uuid[0..5] if self.slug.blank?
+    def assign_slug
+      self.slug = SecureRandom.uuid[0..5] if not_custom_slug?
+    end
+
+    def unassign_slug
+      self.slug = nil
+    end
+
+    def not_custom_slug?
+      @not_custom_slug ||= self.slug.blank? ? true : false
     end
 end
